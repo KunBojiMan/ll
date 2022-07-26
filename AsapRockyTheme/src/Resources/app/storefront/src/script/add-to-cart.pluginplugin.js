7c2ce618045f6c14a08dea510498a045a60be306
Erwin&Kevin <107315000+KunBojiMan@users.noreply.github.com>
2022-07-26 17:16:13 +0200

    Add files via upload

---
+++ AsapRockyTheme/src/Resources/app/storefront/src/script/add-to-cart.pluginplugin.js
@@ -0,0 +1,38 @@
+import AddToCardPlugin from 'src/plugin/add-to-cart/add-to-cart.plugin'
+import DomAccess from 'src/helper/dom-access.helper'
+import HttpClient from 'src/service/http-client.service'
+
+export default class AddToCardPluginPlugin extends AddToCardPlugin {
+    init() {
+            this.PluginManger = window.PluginManager
+            this._cartEl = DomAccess.querySelector(document, '.header-cart')
+            this._client = new HttpClient(window.accessKey, window.contextToken)
+            super.init()
+    }
+
+    _openOffCanvasCart(instance,requestUrl, formData)  {
+        this._client.post(requestUrl, formData, this. afterAddItemToCard.bind(this))
+    }
+
+    _afterAddItemToCart() {
+        this._refreshCartValue()
+        console.info('sasd')
+        this._rumbleButton()
+    }
+
+    _refreshCartValue() {
+        const cartWidgetEl = DomAccess.querySelector(this._cartEl, '[data-cart-widget]')
+        const cartWidgetInstance = this.PluginManger.getPluginIntancefromElement(cartWidgetEl, 'CartWidget')
+        cartWidgetInstance.fetch()
+
+    }
+
+    _rumbleButton() {
+
+        this._cartEl.classList.add('rumble')
+        window.setTimeout(() => {
+            this._cartEl.classList.remove('rumble')
+        }, 3000)
+    }
+
+}
